c714b5cc88d3ffdc660590703e1285514de8bb6a
Job Vonck <job.vonck@prorail.nl>
2026-08-12 11:47:47 +0200

    fix spelling

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,7 @@
       ./hardware-configuration.nix
     ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ]
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
