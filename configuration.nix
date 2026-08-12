2eedb7a18af1150a64a1c37a26e32fac22f7e0a3
Job Vonck <job.vonck@prorail.nl>
2026-08-12 11:36:24 +0200

    add ssh authorized key

--- configuration.nix
+++ configuration.nix
@@ -32,12 +32,25 @@
     LC_TIME = "nl_NL.UTF-8";
   };
 
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      AllowUsers = [ "job" ];
+      MaxAuthTries = 3;
+      PerSourcePenalties = "crash:3600s authfail:3600s max:86400s";
+    };
+  };
+
   users.users."job" = {
     isNormalUser = true;
     description = "job";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYNgjDzDE+ZtmPGdPIRKXBkji3xLmP2m+fETiEeP5/h job@local"
     ];
   };
 
@@ -48,8 +61,6 @@
     git
     fastfetch
   ];
-
-  services.openssh.enable = true;
 
   system.stateVersion = "26.05";
 }
