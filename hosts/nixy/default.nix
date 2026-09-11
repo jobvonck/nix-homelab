c294b6dfd716bf487c2aa467e98867bc953a3858
Job Vonck <job.vonck@prorail.nl>
2026-09-11 19:50:13 +0200

    add battery protection

--- hosts/nixy/default.nix
+++ hosts/nixy/default.nix
@@ -51,6 +51,25 @@
     };
   };
 
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
+      CPU_SCALING_GOVERNOR_ON_BAT = "ondemand"; # schedutil powersave, ondemand
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power"; # power, balance_power
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 100;
+
+      START_CHARGE_THRESH_BAT0 = 50;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     vim
     git
