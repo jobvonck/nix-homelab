801c90c924ded4f7d53cbd992161033059eddd7a
Job Vonck <job.vonck@prorail.nl>
2026-09-12 16:24:35 +0200

    enable pass auth vms

--- hosts/test-vm/default.nix
+++ hosts/test-vm/default.nix
@@ -17,7 +17,7 @@
     enable = true;
     openFirewall = true;
     settings = {
-      PasswordAuthentication = false;
+      PasswordAuthentication = true;
       KbdInteractiveAuthentication = false;
       PermitRootLogin = "no";
       AllowUsers = [ "job" ];
